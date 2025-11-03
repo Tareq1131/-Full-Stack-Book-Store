1d2d249b43dc4f10ace26acabb1cc3440814c3d6
Tareq1131 <tareqislam076@gmail.com>
2025-11-03 15:22:05 +0600

    done home and footer design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="max-w-screen-2xl mx-auto px-4">
+    <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
         {/* left side */}
         <div className="flex items-center md:gap-16 gap-4">
