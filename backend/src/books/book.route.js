abf0330d08883be081034535b91f5394bf213cd8
Tareq1131 <tareqislam076@gmail.com>
2025-11-09 14:43:10 +0600

    complited book model controler and route

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,9 +1,14 @@
 const express = require("express");
 // const { cache } = require("react");
 const Book = require("./book.model");
-const { postABook, getAllBooks } = require("./book.controller");
+const {
+  postABook,
+  getAllBooks,
+  getSingleBook,
+  UpdateBook,
+  deleteABook,
+} = require("./book.controller");
 const router = express.Router();
-
 
 // frontend => backend server => controller => book schema  => database => send to server => back to the frontend
 //post = when submit something fronted to db
@@ -12,10 +17,18 @@ const router = express.Router();
 // delete = when delete something
 
 // post a book
-router.post("/create-book",postABook);
+router.post("/create-book", postABook);
 
 // get all books
 router.get("/", getAllBooks);
 
+// single book endpoint
+router.get("/:id", getSingleBook);
+
+// update book  endpoint
+router.put("/edit/:id", UpdateBook);
+
+// delete a book
+router.delete("/:id", deleteABook);
 
 module.exports = router;
